2e3266c9cc2e734483c3ded765d44e95141e0e33
mika1407 <koneautomaatio@hotmail.com>
2026-05-15 16:17:03 +0300

    delete added

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,33 @@
 import HomeHeader from '@/components/HomeHeader';
-import { globalStyles } from "@/styles/global";
-import { Link } from 'expo-router';
-import { ScrollView, Text, View } from "react-native";
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
+import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <View style={globalStyles.container}>
-        <Text style={globalStyles.title}>MacroZone</Text>
-        <HomeHeader />
-      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Meals
-      </Link>
-      </View>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <MacroGrid meals={meals} />
+      <RecentMeals meals={meals} onDelete={loadMeals} />
     </ScrollView>
   );
 }
-
-
